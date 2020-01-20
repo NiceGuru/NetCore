582238112d7bf8f2a38ebd436e81e03f9dce2fe5
kinglital <kinglital@mail.ru>
2020-01-20 10:17:40 +0300

    Регистрация и вывод в консоль приветствия

---
+++ Metanin/Start_Metanit/Start.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Start_Metanit
+{
+    class Start
+    {
+        static void Main (string [] args)
+        {
+            Console.WriteLine ("Hello World!");
+
+            Console.Write ("Обозначься - ......");
+            string str = Console.ReadLine ();
+            Console.WriteLine ($"Привет, {str}");
+
+            Console.ReadKey ();
+        }
+    }
+}
