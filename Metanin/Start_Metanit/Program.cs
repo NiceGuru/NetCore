8fac8c7b2e5d30e1b3dfd282fe87d61decbfe0fd
kinglital <kinglital@mail.ru>
2020-01-20 09:56:29 +0300

    20_1_20 Начало изучения NetCore

---
+++ Metanin/Start_Metanit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Start_Metanit
+{
+    class Program
+    {
+        static void Main (string [] args)
+        {
+            Console.WriteLine ("Hello World!");
+        }
+    }
+}
